8c497ee736ccd5d198c5463fa53dd5f5e321e487
dannyyeung <752000307@qq.com>
2025-12-29 18:03:53 +0800

    移除3rdparty/freetype submodule，使用SDL3_ttf/external/freetype

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,14 +1,8 @@
 # 全局开关，让所有库都编译为静态库
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 
-add_subdirectory(3rdparty/freetype)
-
-# 告诉 SDL_ttf 用这个
-set(FREETYPE_INCLUDE_DIRS
-    $<TARGET_PROPERTY:freetype,INTERFACE_INCLUDE_DIRECTORIES>)
-
-set(FREETYPE_LIBRARY freetype)
-
+get_property(allTargets GLOBAL PROPERTY TARGETS)
+message(STATUS "All targets: ${allTargets}")
 
 set(SDL_TESTS OFF CACHE BOOL "" FORCE)
 set(SDL_EXAMPLES OFF CACHE BOOL "" FORCE)
@@ -23,7 +17,5 @@ add_subdirectory("${CMAKE_SOURCE_DIR}/3rdparty/SDL3_image")
 set(SDL3TTF_TESTS OFF CACHE BOOL "" FORCE)
 add_subdirectory("${CMAKE_SOURCE_DIR}/3rdparty/SDL3_ttf")
 
-# glm header-only library, change to add git submodule
-# find_package(glm REQUIRED)
 add_subdirectory("${CMAKE_SOURCE_DIR}/3rdparty/glm")
 
